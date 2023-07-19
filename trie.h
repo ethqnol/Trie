cdc615c24ce0c7467ee895ce2f0209f13b709700
ethqnol <112029223+ethqnol@users.noreply.github.com>
2023-07-19 18:56:08 -0400

    asdfasdfasdfasdfg341515234fcdevcvxsdfeiuj8j.n
    Modified the search to include searching for prefix and searching for full word :D

--- trie.h
+++ trie.h
@@ -36,11 +36,11 @@ struct Trie{
         void insert(std::string word){
             Node* temp = root;
             for(char ch : word){
-                if(temp->children[ch - 'a'] == NULL){
+                if(temp->children[ch - 'A'] == NULL){
                     Node* node = new Node(ch);
-                    temp->children[ch - 'a'] = node;
+                    temp->children[ch - 'A'] = node;
                 }
-                temp = temp->children[ch - 'a'];
+                temp = temp->children[ch - 'A'];
             }
             temp->leaf = true;
         }
@@ -48,10 +48,24 @@ struct Trie{
         bool search(std::string word){
             Node* temp = root;
             for(char ch : word){
-                if(temp->children[ch - 'a'] == NULL){
+                if(temp->children[ch - 'A'] == NULL){
                     return false;
                 } else {
-                    temp = temp->children[ch-'a'];
+                    temp = temp->children[ch-'A'];
+                }
+
+            }
+            return temp->leaf;
+        }
+        
+
+        bool searchPrefix(std::string word){
+            Node* temp = root;
+            for(char ch : word){
+                if(temp->children[ch - 'A'] == NULL){
+                    return false;
+                } else {
+                    temp = temp->children[ch-'A'];
                 }
 
             }
